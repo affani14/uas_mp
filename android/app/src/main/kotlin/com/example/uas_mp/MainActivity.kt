9cd81af07eb420c0531e6e28f12cf863964e0f1c
Affani Yusuf <affani14@gmail.com>
2025-01-26 15:38:58 +0700

    update

--- android/app/src/main/kotlin/com/example/uas_mp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/uas_mp/MainActivity.kt
@@ -1,5 +1,21 @@
-package com.example.uas_mp
+package com.example.uas_MP
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.drm"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "initializeDRM") {
+                result.success("DRM Initialized Successfully")
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
